54962dd7ffc70f7f131a6d927d5f18e95508c1f9
Ozod26 <ozodfarid46@gmail.com>
2024-02-26 01:25:13 +0900

    feat: connect mongoDB via mongoose

--- train.ts
+++ train.ts
@@ -1,20 +1,24 @@
-function getHighestIndex(arr) {
-  if (!Array.isArray(arr) || arr.length === 0) {
-      return -1; 
-  }
-  
-  let max = arr[0]; 
-  let maxIndex = 0; 
-  
-  for (let i = 1; i < arr.length; i++) {
-      if (arr[i] > max) {
-          max = arr[i];
-          maxIndex = i;
-      }
-  }
-  
-  return maxIndex;
-}
 
-console.log(getHighestIndex([5, 21, 12, 21, 8])); // Konsolga 1 ni chiqaradi
+
+
+
+// function getHighestIndex(arr) {
+//   if (!Array.isArray(arr) || arr.length === 0) {
+//       return -1; 
+//   }
+  
+//   let max = arr[0]; 
+//   let maxIndex = 0; 
+  
+//   for (let i = 1; i < arr.length; i++) {
+//       if (arr[i] > max) {
+//           max = arr[i];
+//           maxIndex = i;
+//       }
+//   }
+  
+//   return maxIndex;
+// }
+
+// console.log(getHighestIndex([5, 21, 12, 21, 8])); // Konsolga 1 ni chiqaradi
 
