18b194db868221ddb1733de8fb22616d2ae4e99b
Ozod26 <ozodfarid46@gmail.com>
2024-02-28 15:12:18 +0900

    fix: modify routing system and controllers

---
+++ src/router.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const router = express.Router();
+import memberController from "./controllers/member.controller";
+
+
+// router.get("/", restaurantController.goHome);
+
+// router.get("/login", restaurantController.getLogin);
+
+// router.get("/signup", restaurantController.getSignup);
+
+export default router;
