6914daa1d9dbe2e9d58ddbfe2e0e70b8b1718f91
Ozod26 <ozodfarid46@gmail.com>
2024-03-01 16:57:01 +0900

    feat: create restaurand member related routed get and post endpoint

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -6,7 +6,7 @@ const restaurantController: T =  {};
 restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    res.send("Home page");
+    res.send("Home page"); // send | json | redirect | end  | render
   } catch (err) {
     console.log("Error, goHome:", err); 
   }
@@ -29,5 +29,23 @@ restaurantController.getSignup = (req: Request, res: Response) => {
     console.log("Error, getSignup:", err); 
   }
 };
+
+restaurantController.processLogin = (req: Request, res: Response) => {
+  try {
+    console.log("processLogin");
+    res.send("DONE ");
+  } catch (err) {
+    console.log("Error, processLogin:", err); 
+  }
+};
+restaurantController.processSignup = (req: Request, res: Response) => {
+  try {
+    console.log("processSignup");
+    res.send("DONE ");
+  } catch (err) {
+    console.log("Error, processSignup:", err); 
+  }
+};
+
 
 export default restaurantController;
