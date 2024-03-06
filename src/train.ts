b3148e99b2871a7035261cc576d272d1179e2a4e
Ozod26 <ozodfarid46@gmail.com>
2024-03-06 17:52:15 +0900

    train bajarildi

--- src/train.ts
+++ src/train.ts
@@ -1,4 +1,16 @@
 
+/** 
+ * I-TASK: 
+Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+MASALAN: getDigits("m14i1t") return qiladi "141"
+ */
+
+function getDigits(str: string) {
+  return str.replace(/\D/g, "");
+}
+
+console.log(getDigits("m14i1t")); 
+
 /* Project Standards:
   - Logging standards:
   - Naming standards:
